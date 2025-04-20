7eab11f915613d248a5b79d1694ecfc83a89899d
Ankur Patel <asanaconsultingcorp@gmail.com>
2025-04-20 07:58:51 -0400

    testing dataframe to dictionary functionality

--- app-test.py
+++ app-test.py
@@ -1,3 +1,17 @@
-df_uuid = snowpark_session.sql('SELECT UUID_STRING()').collect()
+#2) TEST DATAFRAME TO DICTIONARY
+#import pandas as pd
 
-print(df_uuid)
+# Sample DataFrame
+#df = pd.DataFrame({
+#    'A': [1, 2, 3],
+#    'B': [4, 5, 6]
+#})
+
+# Convert DataFrame to dictionary using zip
+#dict_from_columns = dict(zip(df.columns, df.T.values.tolist()))
+#print(df)
+#print(dict_from_columns)
+
+#1) UUID TEST
+#df_uuid = snowpark_session.sql('SELECT UUID_STRING()').collect()
+#print(df_uuid)
